03030340bc7f0ace2762e9f871c0c1c5d7abd268
jensenhuangyankai <jensen@huangyankai.com>
2025-03-25 00:57:24 +0800

    Fix SampleDataUtil.java initialisation

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -11,6 +11,8 @@ import seedu.address.model.person.Email;
 import seedu.address.model.person.Name;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.Phone;
+import seedu.address.model.person.category.DietaryRestriction;
+import seedu.address.model.person.category.Rsvp;
 import seedu.address.model.tag.Tag;
 
 /**
@@ -21,22 +23,28 @@ public class SampleDataUtil {
         return new Person[] {
             new Person(new Name("Alex Yeoh"), new Phone("87438807"), new Email("alexyeoh@example.com"),
                 new Address("Blk 30 Geylang Street 29, #06-40"),
-                getTagSet("friends"), null, null, null),
+                getTagSet("friends"), new DietaryRestriction(DietaryRestriction.TypicalRestriction.NONE), 
+                new Rsvp(Rsvp.Status.NO_RESPONSE), null),
             new Person(new Name("Bernice Yu"), new Phone("99272758"), new Email("berniceyu@example.com"),
                 new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"),
-                getTagSet("colleagues", "friends"), null, null, null),
+                getTagSet("colleagues", "friends"), new DietaryRestriction(DietaryRestriction.TypicalRestriction.VEGETARIAN), 
+                new Rsvp(Rsvp.Status.YES), null),
             new Person(new Name("Charlotte Oliveiro"), new Phone("93210283"), new Email("charlotte@example.com"),
                 new Address("Blk 11 Ang Mo Kio Street 74, #11-04"),
-                getTagSet("neighbours"), null, null, null),
+                getTagSet("neighbours"), new DietaryRestriction(DietaryRestriction.TypicalRestriction.HALAL), 
+                new Rsvp(Rsvp.Status.NO), null),
             new Person(new Name("David Li"), new Phone("91031282"), new Email("lidavid@example.com"),
                 new Address("Blk 436 Serangoon Gardens Street 26, #16-43"),
-                getTagSet("family"), null, null, null),
+                getTagSet("family"), new DietaryRestriction(DietaryRestriction.TypicalRestriction.PEANUTS), 
+                new Rsvp(Rsvp.Status.YES), null),
             new Person(new Name("Irfan Ibrahim"), new Phone("92492021"), new Email("irfan@example.com"),
                 new Address("Blk 47 Tampines Street 20, #17-35"),
-                getTagSet("classmates"), null, null, null),
+                getTagSet("classmates"), new DietaryRestriction(DietaryRestriction.TypicalRestriction.VEGAN), 
+                new Rsvp(Rsvp.Status.NO_RESPONSE), null),
             new Person(new Name("Roy Balakrishnan"), new Phone("92624417"), new Email("royb@example.com"),
                 new Address("Blk 45 Aljunied Street 85, #11-31"),
-                getTagSet("colleagues"), null, null, null)
+                getTagSet("colleagues"), new DietaryRestriction(DietaryRestriction.TypicalRestriction.SHELLFISH), 
+                new Rsvp(Rsvp.Status.NO), null)
         };
     }
 
