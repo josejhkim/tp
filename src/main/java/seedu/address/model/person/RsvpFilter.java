816bbed1d96de283cffced4e7618ad9f6db3afb3
Jonathan Ngien <jonathan.ngien.zy@gmail.com>
2025-03-24 16:34:24 +0800

    Adds test for filterGuest functionality

--- src/main/java/seedu/address/model/person/RsvpFilter.java
+++ src/main/java/seedu/address/model/person/RsvpFilter.java
@@ -1,22 +1,23 @@
 package seedu.address.model.person;
+
 import java.util.function.Predicate;
 
 import seedu.address.commons.util.ToStringBuilder;
 
- /**
-  * A predicate that checks if a {@link Person}'s RSVP status matches a specified RSVP.
-  * <p>
-  * This predicate is used to filter {@code Person} objects based on their RSVP status.
-  * It implements the {@link java.util.function.Predicate} interface, enabling it to be used
-  * in stream filtering and other contexts where a predicate is required.
-  * </p>
-  * <p>
-  * The predicate evaluates to {@code true} if the RSVP obtained from {@link Person#getRsvp()}
-  * equals the RSVP provided to this filter, and {@code false} otherwise.
-  * </p>
-  *
-  * @see Person#getRsvp()
-  */
+/**
+ * A predicate that checks if a {@link Person}'s RSVP status matches a specified RSVP.
+ * <p>
+ * This predicate is used to filter {@code Person} objects based on their RSVP status.
+ * It implements the {@link java.util.function.Predicate} interface, enabling it to be used
+ * in stream filtering and other contexts where a predicate is required.
+ * </p>
+ * <p>
+ * The predicate evaluates to {@code true} if the RSVP obtained from {@link Person#getRsvp()}
+ * equals the RSVP provided to this filter, and {@code false} otherwise.
+ * </p>
+ *
+ * @see Person#getRsvp()
+ */
 public class RsvpFilter implements Predicate<Person> {
     private final Rsvp rsvpFilter;
 
