8a8978fef9f3cdf2784be9ff38fa7eed12badb0d
Jonathan Ngien <jonathan.ngien.zy@gmail.com>
2025-03-24 08:52:18 +0800

    Add tests to RSVP and dietary restriction filter

--- src/test/java/seedu/address/logic/commands/FilterGuestCommandTest.java
+++ src/test/java/seedu/address/logic/commands/FilterGuestCommandTest.java
@@ -1,4 +1,15 @@
 package seedu.address.logic.commands;
 
+import org.junit.jupiter.api.BeforeEach;
+
+import seedu.address.model.Model;
+
 public class FilterGuestCommandTest {
+
+    private Model model;
+
+    @BeforeEach
+    public void setUp() {
+        model = new ModelSt
+    }
 }
