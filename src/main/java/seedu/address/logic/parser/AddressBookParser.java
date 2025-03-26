11f45c23ce2f0a90cc987c3917e13e7cff473402
Jonathan Ngien <jonathan.ngien.zy@gmail.com>
2025-03-26 21:44:13 +0800

    Fix syntax

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -126,10 +126,10 @@ public class AddressBookParser {
 
         case FindTableCommand.COMMAND_WORD:
             return new FindTableCommandParser().parse(arguments);
+
         default:
             logger.finer("This user input caused a ParseException: " + userInput);
             throw new ParseException(MESSAGE_UNKNOWN_COMMAND);
         }
     }
-
 }
