d03a19accc72509460f7eb5c6f89a807e5da4caa
Kaung Z Y Naung <kaungzinye@Kaungs-Laptop.local>
2025-04-01 14:52:44 +0800

    Change tableId/ to tid/

--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -11,12 +11,8 @@ public class CliSyntax {
     public static final Prefix PREFIX_EMAIL = new Prefix("e/");
     public static final Prefix PREFIX_ADDRESS = new Prefix("a/");
     public static final Prefix PREFIX_TAG = new Prefix("t/");
-    public static final Prefix PREFIX_GUEST_ID = new Prefix("gid/");
     public static final Prefix PREFIX_DIETARY_RESTRICTION = new Prefix("d/");
     public static final Prefix PREFIX_RSVP = new Prefix("r/");
-    public static final Prefix PREFIX_CATEGORY = new Prefix("c/");
-    public static final Prefix PREFIX_FIELD = new Prefix("f/");
     public static final Prefix PREFIX_TABLE_ID = new Prefix("tableId/");
     public static final Prefix PREFIX_CAPACITY = new Prefix("capacity/");
-    public static final Prefix PREFIX_WEDDING_NAME = new Prefix("weddingName/");
 }
